5e1c09001022c68ebd25a168b533783a865f580b
Daniel Evans <evansdb0@sewanee.edu>
2016-06-23 23:33:37 -0500

    Documented insertion sort further

--- java/edu/evansdaniel/collections/SortingUtils.java
+++ java/edu/evansdaniel/collections/SortingUtils.java
@@ -13,15 +13,29 @@ public class SortingUtils {
         }
     }
 
+    /**
+     * 'new' elements are defined as a[j]
+     * 'old' elements are defined as a[j-1...0]
+     *
+     * @param a any array
+     */
     public static void insertionSort(int[] a) {
         for (int j = 1; j < a.length; ++j) {
+            // save value of 'new' element
             int key = a[j];
 
             int i = j - 1;
+            // loop through the 'old' indices
             while (i >= 0 && a[i] > key) {
+                // make the next element be the current element, i.e. shift the
+                // current element over; this is okay because we have saved the
+                // value of the 'new' index in key
                 a[i + 1] = a[i];
+                // decrement i, will make it to -1
                 --i;
             }
+            // will 'insert' the 'new' element either at the beginning or
+            // at the point where the key is greater than a[i]
             a[i + 1] = key;
         }
     }
