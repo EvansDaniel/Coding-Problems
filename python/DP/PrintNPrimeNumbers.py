5a8bfc70aa916f6f71523a306c81b65be29c8f37
Daniel Evans <evansdb0@sewanee.edu>
2016-07-21 23:15:45 -0500

    Implemented an algorithm to find and print the first n prime numbers

---
+++ python/DP/PrintNPrimeNumbers.py
@@ -0,0 +1,28 @@
+def print_first_n_prime_numbers(n):
+    count = 0
+    # i is the current number we are checking for "primeness"
+    i = 2
+    # iterate until we have printed n prime numbers
+    while count < n:
+        # p stands for prime, assume its true
+        p = True
+        # loop through 2 - 9 because if i is not divisible by any of these numbers then
+        # i (is prime) will not be divisible by any numbers greater than 9 either
+        for j in range(2, 9):
+            # if j == i that means that i is prime
+            if j == i:
+                break
+            # if i is divisible by j than it is not prime and stop loop
+            if i % j == 0:
+                p = False
+                break
+
+        # check if i was found to be prime
+        if p:
+            print i,
+            # increment count since it keeps track of the number of prime numbers found
+            count += 1
+        i += 1
+
+
+print_first_n_prime_numbers(10)
