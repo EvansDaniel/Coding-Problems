97e193c5d452e1d48d17979ab7a75a3078bf3fbb
Daniel Evans <evansdb0@sewanee.edu>
2016-08-24 10:53:14 -0500

    Added hackerrank dir to cpp to post NextTopologicalOrdering cpp solution

--- java/evansdaniel/hackerrank/NextTopologicalOrdering.java
+++ java/evansdaniel/hackerrank/NextTopologicalOrdering.java
@@ -8,7 +8,6 @@ import java.util.*;
  * Created by daniel on 8/23/16.
  *
  * @author Daniel Evans
- *         Question Prompt: https://www.hackerrank.com/contests/101hack40/challenges/next-topological-sorting
  */
 public class NextTopologicalOrdering {
 
@@ -49,57 +48,17 @@ public class NextTopologicalOrdering {
         // ------------- START ------------------------
         List<Integer> noDepVerts = findNoDepVerts(numVertices, incEdges);
         List<Integer> nextTop = new ArrayList<>(p.size());
-        // iterate over values in p
-        for (int i = 0; i < p.size(); ++i) {
-            Integer minVert = Collections.min(noDepVerts);
-            if (noDepVerts.size() > 1) {
-                // add min vertice to next topological sort
-                nextTop.add(minVert);
-                // add all new no dependency verts to list
-                noDepVerts.addAll(nt(noDepVerts, nextTop, minVert));
-            } else {
-                // add minVert to list remove from deps
-                nextTop.add(minVert);
-                noDepVerts.remove(minVert);
-            }
-        }
-    }
 
-    private static Collection<? extends Integer> nt
-            (List<Integer> noDepVerts, List<Integer> nextTop, Integer minVert) {
-
-        noDepVerts.remove(minVert);
-        for (int i = 0; i < noDepVerts.size(); ++i) {
-            noDepVerts.addAll(getNoDepVerts(minVert, nextTop));
-        }
-        return new ArrayList<>();
     }
 
     private static List<Integer> findNoDepVerts
-            (int numVerts,
-             Hashtable<Integer, List<Integer>> incEdges) {
+            (int numVerts, Hashtable<Integer, List<Integer>> incEdges) {
         List<Integer> noDepVerts = new ArrayList<>();
         for (int i = 1; i < numVerts + 1; i++) {
-            if (incEdges.get(i).size() == 0) {
+            if (NextTopologicalOrdering.incEdges.get(i).size() == 0) {
 //                System.out.println(i);
                 noDepVerts.add(i);
             }
         }
         return noDepVerts;
     }
-
-    public static Collection<? extends Integer> getNoDepVerts
-            (Integer minVert, List<Integer> nextTop) {
-        List<Integer> noDepVerts = new ArrayList<>();
-        List<Integer> adjVert = adjVerts.get(minVert);
-        for (int i = 0; i < adjVert.size(); i++) {
-            boolean notDependent = false;
-            for (Integer integer : incEdges.get(i)) {
-                notDependent = nextTop.contains(integer);
-            }
-            if (notDependent)
-                noDepVerts.add(i);
-        }
-        return noDepVerts;
-    }
-}
