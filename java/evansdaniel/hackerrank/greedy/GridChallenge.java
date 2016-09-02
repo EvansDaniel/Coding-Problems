7e7586195652d7bedb88e02f15b3dd56de8f3805
Daniel Evans <evansdb0@sewanee.edu>
2016-09-02 12:11:55 -0500

    Solved Grid Challenge problem on Hackerrank.com (see src for link to problem)

--- java/evansdaniel/hackerrank/greedy/GridChallenge.java
+++ java/evansdaniel/hackerrank/greedy/GridChallenge.java
@@ -4,6 +4,7 @@ import evansdaniel.hackerrank.Config;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -18,22 +19,39 @@ public class GridChallenge {
         int tests, lines;
         Scanner s = new Scanner(new File(Config.getDataDir() + "GridChallenge"));
         tests = s.nextInt();
-        lines = s.nextInt();
-        int[][] grid = new int[lines][lines];
+        while (tests-- > 0) {
+            lines = s.nextInt();
+            char[][] grid = new char[lines][lines];
 
-        String nextLine = null;
-        for (int i = 0; i < grid.length; i++) {
-            nextLine = s.next();
-            System.out.println(nextLine);
-            for (int j = 0; j < grid.length; j++) {
-                grid[i][j] = nextLine.charAt(j);
+            String nextLine = null;
+            for (int i = 0; i < grid.length; i++) {
+                nextLine = s.next();
+                for (int j = 0; j < grid.length; j++) {
+                    grid[i][j] = nextLine.charAt(j);
+                }
             }
-        }
         /*for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid.length; j++) {
                 System.out.printf("%c ", grid[i][j]);
             }
             System.out.println();
         }*/
+            for (char[] chars : grid) {
+                Arrays.sort(chars);
+                System.out.println(Arrays.toString(chars));
+            }
+            boolean isSortedCorrectly = true;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 1; j < grid.length; j++) {
+                    System.out.print(grid[j][i] + " > " + grid[j - 1][i] + " ");
+                    System.out.println();
+                    if (!(grid[j][i] >= grid[j - 1][i])) {
+                        isSortedCorrectly = false;
+                        break;
+                    }
+                }
+            }
+            System.out.println(isSortedCorrectly ? "YES" : "NO");
+        }
     }
 }
