5a9a53961b1cbcce6dbc58c5076a914236a45359
Daniel <evansdb0@sewanee.edu>
2016-09-07 13:19:07 -0500

    Solved the Red John Is Back problem on hackerrank.com (see src for link to problem)

---
+++ cpp/Config.h
@@ -0,0 +1,18 @@
+//
+// Created by daniel on 9/7/16.
+//
+
+#ifndef CPPTEST_CONFIG_H
+#define CPPTEST_CONFIG_H
+
+#include <string>
+
+
+class Config {
+
+public:
+    static std::string getDataDir();
+};
+
+
+#endif //CPPTEST_CONFIG_H
