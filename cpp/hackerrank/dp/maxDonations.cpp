fa74bbfb379105e015f1a7a57a537f30c924093a
Daniel <evansdb0@sewanee.edu>
2016-09-13 23:57:20 -0500

    Solved maxDonations problem on topcoder.com (see source for link to problem)

--- cpp/hackerrank/dp/maxDonations.cpp
+++ cpp/hackerrank/dp/maxDonations.cpp
@@ -1,27 +1,24 @@
 // Author: Daniel Evans
 // Problem Statement: https://community.topcoder.com/stat?c=problem_statement&pm=2402&rd=5009
-
 #include <iostream>
 #include <vector>
 #include <map>
 
+
 using namespace std;
+map<int, int> dp;
+vector<int> don;
+map<int, int> p;
 
-map<int, int> answer;
-vector<int> donations;
-
-int maxDon(int i1, int i2) {
-
-    int key = i1 * 500 + i2;
-    if (answer.count(key)) return answer[key];
-    if (i1 > i2) return 0;
-    answer[key] = max(maxDon(i1 + 1, i2), maxDon(i1 + 2, i2) + donations[i1]);
-    return answer[key];
+int maxDon(int i1, int end) {
+    int key = i1 * 500 + end;
+    if (dp.count(key)) return dp[key];
+    if (i1 > end) return 0;
+    dp[key] = max(maxDon(i1 + 1, end), don[i1] + maxDon(i1 + 2, end));
+    return dp[key];
 }
-
-
 int main() {
-    donations = {1, 3, 65, 239, 49, 239, 209};
-    int n = donations.size();
-    std::cout << max(maxDon(0, n - 2), maxDon(1, n - 1));
+    don = {10, 20, 30, 40, 50, 60};
+    int n = don.size();
+    cout << max(maxDon(0, n - 2), maxDon(1, n - 1));
 };
